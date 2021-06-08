8cf14ad00dddd341546b1ef4d958721266aecba0
alan_yeung <yeunglun1125@gmail.com>
2021-06-08 15:00:45 +0800

    [Search bar] Added placeholder, fixed style

--- hiroba_plugin.js
+++ hiroba_plugin.js
@@ -10,7 +10,7 @@ function createMainArea(){
 }
 
 function searching(){
-    var buttonsHtml = "<div id='myplugin_search_bar' style='position: fixed;bottom: 0;z-index: 1;background: #FF7F00;padding: 4px;width: 292px;'><input style='width:284px;' margin:4px; border-radius: 4px;type='text'";
+    var buttonsHtml = "<div id='myplugin_search_bar' style='position: fixed;bottom: 0;z-index: 1;background: #FF7F00;padding: 4px;width: 292px;'><input placeholder='請輸入歌名 Please type song name' style='width:278px; margin:4px; border-radius: 4px;'type='text'";
     buttonsHtml += "</div>";
     $('#myplugin_main').append(buttonsHtml);
     
