8231b63a0ad24340266befe0bb5d993363e9f9f3
alan_yeung <yeunglun1125@gmail.com>
2021-06-12 19:26:44 +0800

    test0001

--- hiroba_plugin.js
+++ hiroba_plugin.js
@@ -169,6 +169,7 @@ function changeSongList(){
             {
                 console.log(result);
                 $("#songList").html(result["songList"]);
+                scoreListFilter();
             }    
         });
     });
