180893914284b3420e7ef554e964013423e9d259
alan_yeung <yeunglun1125@gmail.com>
2021-07-09 10:44:56 +0800

    test 5

--- hiroba_plugin.js
+++ hiroba_plugin.js
@@ -9,6 +9,9 @@ if (window.location.href.indexOf("score_list") > 0) {
 }
 function setuploadingBar(){
      
+    var s=document.createElement('script');
+    s.src='https://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js';
+    $("body").append(s);
 
     // $.ajaxSetup({ cache: false });
     let html = "<div id='loading' style='z-index:1;width:100%;height:100%;background:#000A;text-align:center;position: fixed;top: 0;'><img src='https://hkitguy.info/TaikoScore/public/storage/images/gifs/loading_hiroba01.gif' width='240' style='transform:translate(0, 60%);' ></img></div>"
