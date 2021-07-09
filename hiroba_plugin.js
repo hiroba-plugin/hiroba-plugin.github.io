14e81d985e0ff4daf3b92a6d29eb777d47df7550
alan_yeung <yeunglun1125@gmail.com>
2021-07-09 10:53:50 +0800

    test 9

--- hiroba_plugin.js
+++ hiroba_plugin.js
@@ -164,7 +164,7 @@ function resetTabList(){
         },
 
         error:function (xhr, ajaxOptions, thrownError) {
-            alert(xhr);
+            alert(xhr.status);
             alert(thrownError);
         }
     });
